a92b1ed2dda596da1745fee250a4e492b7068ccf
jhonf <jhonfisc@gmail.com>
2020-12-13 16:03:41 -0500

    delete dir mocks

--- src/mocks/__customer_mock.js
+++
@@ -1,8 +0,0 @@
-const customer = {
-    "referencia": "100", // hdvcodigo
-    "nombre": "Legovo",    // hdvnommbre + hdvapellid
-    "identificacion": "80000000",
-    "digito-verificacion": "07"
-};
-
-export { customer };
